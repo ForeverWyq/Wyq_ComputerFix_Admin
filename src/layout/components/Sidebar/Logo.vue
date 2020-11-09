ac1ecc9890fa16c1ebc779ba0fc13d02558f4dd4
ForeverWyq <987043378@qq.com>
2020-11-09 19:42:06 +0800

    更新logo

--- src/layout/components/Sidebar/Logo.vue
+++ src/layout/components/Sidebar/Logo.vue
@@ -25,7 +25,8 @@ export default {
   data() {
     return {
       title: '电脑维修保养预约平台',
-      logo: 'https://wpimg.wallstcn.com/69a1c46c-eb1c-4b46-8bd4-e9e686ef5251.png'
+      // logo: 'https://wpimg.wallstcn.com/69a1c46c-eb1c-4b46-8bd4-e9e686ef5251.png'
+      logo: 'http://223.4.183.62/ComputerFix/images/logo/logo.png'
     }
   }
 }
